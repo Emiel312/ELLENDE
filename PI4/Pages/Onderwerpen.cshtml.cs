7c096df9701211b5b9fbd9bb4cc866ef26fb2651
Emiel <emielbaur@outlook.com>
2023-03-17 11:42:11 +0100

    functionaliteit Youtubelinks pagina toegevoegd

--- PI4/Pages/Onderwerpen.cshtml.cs
+++ PI4/Pages/Onderwerpen.cshtml.cs
@@ -9,7 +9,7 @@ namespace PI4.Pages
     public class OnderwerpenModel : PageModel
     {
         public IEnumerable<Onderwerp> onderwerpen { get; set; } = null!;
-
+        public string OmschrijvingSort { get; set; }
 
         private VideoSubjectDbContext db;
         public OnderwerpenModel(VideoSubjectDbContext injectedContext)
@@ -19,8 +19,18 @@ namespace PI4.Pages
 
         public void OnGet(string sortOrder)
         {
-            onderwerpen = db.Onderwerpen.ToList();
+            onderwerpen = db.Onderwerpen.OrderBy(s => s.Omschrijving).ToList();
+            OmschrijvingSort = sortOrder == "omschrijving" ? "omschrijving_desc" : "omschrijving";
 
+            switch (sortOrder)
+            {
+                case "omschrijving_desc":
+                    onderwerpen = onderwerpen.OrderByDescending(s => s.Omschrijving).ToList();
+                    break;
+                case "omschrijving":
+                    onderwerpen = onderwerpen.OrderBy(s => s.Omschrijving).ToList();
+                    break;
+            }
         }
     }
 }
--- PI4/Pages/YouTubeLinks.cshtml.cs
+++ PI4/Pages/YouTubeLinks.cshtml.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PI4.Data;
+using Microsoft.EntityFrameworkCore;
+using PI4.Models;
 
 namespace PI4.Pages
 {
     public class YouTubeLinksModel : PageModel
     {
-        public void OnGet()
+        public IEnumerable<Video> videos { get; set; } = null!;
+        public string TitelSort { get; set; }
+
+        private VideoSubjectDbContext db;
+        public YouTubeLinksModel(VideoSubjectDbContext injectedContext)
         {
+            db = injectedContext;
+        }
+
+        public void OnGet(string sortOrder)
+        {
+            videos = db.Videos.OrderBy(s => s.Titel).ToList();
+            TitelSort = sortOrder == "titel" ? "titel_desc" : "titel";
+
+            switch (sortOrder)
+            {
+                case "titel_desc":
+                    videos = videos.OrderByDescending(s => s.Titel).ToList();
+                    break;
+                case "titel":
+                    videos = videos.OrderBy(s => s.Titel).ToList();
+                    break;
+            }
         }
     }
 }
